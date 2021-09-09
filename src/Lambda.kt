4ca98db44393f049607f9673b58caa408cab72ee
Al-AminJoy <alaminislam3555@gmail.com>
2021-09-09 18:00:21 +0600

    Lambda Expression

--- src/Lambda.kt
+++ src/Lambda.kt
@@ -3,8 +3,47 @@ fun main(args: Array<String>) {
     printMessage("Hello");
 
     val sayHello = { message:String, name:String ->
-        print(message+" "+name);
+        println(message+" "+name);
     };
 
     sayHello("Hello","Joy");
+
+    fun downloadData (url:String, completion:()-> Unit){
+        //start download
+        //complete download
+        completion();
+    }
+    downloadData("www.fakeUrl.com") { println("After Fake download Complete") }
+
+
+    fun downloadCarData (url:String,completion:(Car) -> Unit){
+        //start download
+        //complete download
+        val car = Car("Tesla","Node");
+
+        completion (car);
+    }
+
+    downloadCarData("FakeUrl") { car ->
+        println(car.make) ;
+        println(car.model);
+    }
+
+    fun downloadCarInfo (url:String , completion :(Car?,Boolean)->Unit){
+        val colored = true;
+        if (colored){
+            val car = Car("Tesla","Fade");
+            completion(car,true)
+        }else{
+            completion(null,false);
+        }
+    }
+
+    downloadCarInfo("FakeUrl"){ car,success->
+        if (success){
+            println(car?.model+" Car Colored");
+        }else{
+            println(car?.model+" Car Not Colored");
+        }
+    }
 }
